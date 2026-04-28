d36d70545bb78efdb1df11bb211bf87f270464ab
AlinorSangcopan <sangcopan.alinor321@gmail.com>
2026-04-28 03:50:36 +0000

    PLATOON LEADER

--- MISSION_001.py
+++ MISSION_001.py
@@ -1,10 +1,23 @@
 try:
     f = open("diary.txt", "x")
     f.write("Military Diary")
-    print("File created!")
+    print("FILE CREATED!")
 except FileExistsError:
-    print("File already exists!")
+    print("FILE ALREADY EXISTS!")
 
+while True:
+    print("--------------------")
+    print("|  Military Diary  |")
+    print("--------------------")
+    print("[1] ADD DIARY")
+    print("[2] VIEW DIARY")
+    print("[3] SEARCH DIARY DAY")
+    print("[4] DELETE DIARY DAY")
+    print("[5] EXIT")
 
+    option = input("ENTER OPTION: ")
 
-    
+    if option == "1":
+        with open("diary.txt", "a") as file:
+            file.write(input("ENTER YOUR DIARY: "))
+            print("MISSION COMPLETE!")
